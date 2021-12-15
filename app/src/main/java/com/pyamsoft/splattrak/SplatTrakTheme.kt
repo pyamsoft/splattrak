6d47288758337d37b346b959456e7ad5893dd2d8
pyamsoft <developer.pyamsoft@gmail.com>
2021-12-14 21:36:09 -0800

    Apply variant theme

--- app/src/main/java/com/pyamsoft/splattrak/SplatTrakTheme.kt
+++ app/src/main/java/com/pyamsoft/splattrak/SplatTrakTheme.kt
@@ -50,6 +50,9 @@ private fun themeColors(isDarkMode: Boolean): Colors {
           onPrimary = onPrimary,
           secondary = secondary,
           onSecondary = onSecondary,
+          // Must be specified for things like Switch color
+          primaryVariant = primary,
+          secondaryVariant = secondary,
       )
   else
       lightColors(
@@ -57,6 +60,9 @@ private fun themeColors(isDarkMode: Boolean): Colors {
           onPrimary = onPrimary,
           secondary = secondary,
           onSecondary = onSecondary,
+          // Must be specified for things like Switch color
+          primaryVariant = primary,
+          secondaryVariant = secondary,
       )
 }
 
